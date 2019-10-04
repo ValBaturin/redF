90cd1e19aa7c962d19634feb2706d0f4e4d82983
Alex Ryndin <alexnryndin@gmail.com>
2019-10-04 13:32:29 +0300

    Use editline for line reading and history

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+
 
 // Declare a buffer for user input of size 2048
-static char input[2048];
 
 int main(int argc, char** argv) {
 
     // Print Version and Exit Information
-    puts("lis v0.1");
+    puts("lis v0.1.1");
     puts("Press ^C to Exit\n");
 
     while(1) {
-        fputs("> ", stdout);
-        fgets(input, 2048, stdin);
-        printf("< %s", input);
+        char* input = readline("> ");
+        add_history(input);
+        printf("< %s\n", input);
+        free (input);
     }
 
     return 0;
