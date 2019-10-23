5f1e39fcfa7fac85a0966e88191682f47ffde6cd
Val <valeryxbaturin@gmail.com>
2019-10-23 19:56:43 +0300

    Create Lexer the Iterator

--- redF/lexer.rs
+++ redF/lexer.rs
@@ -16,6 +16,57 @@ enum Number {
     Real (f64)
 }
 
+struct Lexer<'a> {
+    source: std::iter::Peekable<&'a mut std::str::Chars<'a>>,
+}
+
+impl<'a> Lexer<'a> {
+    fn new(s: &'a mut std::str::Chars<'a>) -> Lexer<'a> {
+        Lexer { source: s.peekable() }
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+    while let Some(&c) = self.source.peek() {
+
+        match c {
+            '0'...'9' => {
+                self.source.next();
+                let number = get_number(c, &mut self.source);
+                return Some(Token::Number(number.unwrap()));
+            }
+            'a' ... 'z' => {
+                self.source.next();
+                let symbol = get_symbol(c, &mut self.source);
+                return Some(Token::Symbol(symbol.unwrap()));
+            }
+            '(' => {
+                self.source.next();
+                return Some(Token::Lparen);
+            }
+            ')' => {
+                self.source.next();
+                return Some(Token::Rparen);
+            }
+            '\'' => {
+                self.source.next();
+                return Some(Token::Quote);
+            }
+            ' ' => {
+                self.source.next();
+            }
+            '\n' => {return None}
+            _ => {
+                return None;
+            }
+        }
+    }
+    return None;
+}
+}
 fn get_number<T: Iterator<Item = char>>(c: char, iter: &mut std::iter::Peekable<T>) -> Option<Number> {
     let mut number = c.to_string().parse::<i64>().expect("The caller should have passed a digit.");
     while let Some(Ok(digit)) = iter.peek().map(|c| c.to_string().parse::<i64>()) {
@@ -52,52 +103,17 @@ fn get_symbol<T: Iterator<Item = char>>(c: char, iter: &mut std::iter::Peekable<
     return Some(result);
 }
 
-fn lexer(input: &String) -> Result<Vec<Token>, String> {
-    let mut result = Vec::new();
-
-    let mut it = input.chars().peekable();
-    while let Some(&c) = it.peek() {
-
-        match c {
-            '0'...'9' => {
-                it.next();
-                let number = get_number(c, &mut it);
-                result.push(Token::Number(number.unwrap()));
-            }
-            'a' ... 'z' => {
-                it.next();
-                let symbol = get_symbol(c, &mut it);
-                result.push(Token::Symbol(symbol.unwrap()));
-            }
-            '(' => {
-                result.push(Token::Lparen);
-                it.next();
-            }
-            ')' => {
-                result.push(Token::Rparen);
-                it.next();
-            }
-            '\'' => {
-                result.push(Token::Quote);
-                it.next();
-            }
-            ' ' => {
-                it.next();
-            }
-            '\n' => {return Ok(result)}
-            _ => {
-                return Err(format!("unexpected character {}", c));
-            }
-        }
-    }
-    Ok(result)
-}
-
 fn main() {
-    use std::io::{stdin,stdout,Write};
+    use std::io::stdin;
+
     loop {
-    let mut s=String::new();
-    stdin().read_line(&mut s).expect("Did not enter a correct string");
-    println!("Tokenized:\n{:?}", lexer(&s));
+        let mut s = String::new();
+        stdin().read_line(&mut s).expect("Did not enter a correct string");
+        let mut source = s.chars();
+
+        let lexer = Lexer::new(&mut source);
+        for token in lexer {
+            println!("{:?}", token);
+        }
     }
 }
